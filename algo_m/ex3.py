81400f06c54b07a1e022f8bca6c921aa215e5c6f
LaisAlves55 <if24.lais@gmail.com>
2025-03-20 14:44:48 -0400

    Add files via upload

---
+++ algo_m/ex3.py
@@ -0,0 +1,9 @@
+
+numero1=int(input("Insira o primeiro numero:"))
+numero2=int(input("Insira o segundo numero:"))
+adicao= numero1+numero2
+subtracao= numero1-numero2
+multiplicaçao= numero1*numero2
+print (f"A soma dos numero é:{adicao}, a subtracao é: {subtracao}, a multiplicaçao é: {multiplicaçao}")
+
+
